38e28d8a9a6301c79cb6e8779c7a71bc99ecda70
Allen Xavier Arasan <allen@Allens-MacBook-Air.local>
2025-01-27 18:47:27 +0530

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+// main.jsx
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import Portfolio from './App.jsx'  // Change this line
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Portfolio />  // And this line
+  </StrictMode>
 )
